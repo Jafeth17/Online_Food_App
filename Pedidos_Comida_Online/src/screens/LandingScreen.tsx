7bb51a9a2b7109dc955679fa79940c33f98debfc
Jafeth17 <jafethm38@gmail.com>
2023-07-20 00:13:36 -0600

    Agregando Imagenes

---
+++ Pedidos_Comida_Online/src/screens/LandingScreen.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { View, Text, StyleSheet, Dimensions, Image } from 'react-native';
+
+export const LandingScreen = () => {
+
+    return (
+        <View>
+            <Text> Pagina Inicio </Text>
+        </View>
+    )
+    
+}
+
+const styles = StyleSheet.create({
+
+    container: {
+        flex:1,
+        backgroundColor: 'green'
+    },
+})
