b6e1fced6dbfc5e9ad3cf155cb01a6aca198a80e
Jafeth17 <jafethm38@gmail.com>
2023-07-20 20:01:54 -0600

    Landing page diseno

--- Pedidos_Comida_Online/src/screens/LandingScreen.tsx
+++ Pedidos_Comida_Online/src/screens/LandingScreen.tsx
@@ -1,11 +1,24 @@
 import React from 'react';
 import { View, Text, StyleSheet, Dimensions, Image } from 'react-native';
 
+const screenWidth = Dimensions.get('screen').width
+
 export const LandingScreen = () => {
 
     return (
-        <View>
-            <Text> Pagina Inicio </Text>
+        <View style={styles.container}>
+            <View style={styles.navigation}>
+                
+            </View>
+            <View style={styles.body}>
+                <Image source={require('../images/delivery_icon.png')} style={styles.deliveryIcon} />
+                <View style={styles.addressContainer}>
+                    <Text style={styles.addressTitle}>Tu Dirección de Entrega</Text>
+                </View>
+
+                <Text style={styles.addressText}>Colonia El Higo, Siguatepeque, Comayagua</Text>
+            </View>
+            <View style={styles.footer}/>
         </View>
     )
     
@@ -15,6 +28,39 @@ const styles = StyleSheet.create({
 
     container: {
         flex:1,
-        backgroundColor: 'green'
+        backgroundColor: 'rgba(242,242,242,1)'
     },
+    navigation: {
+        flex: 2,
+    },
+    body: {
+        flex: 9,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    deliveryIcon: {
+        width: 120,
+        height: 120
+    },
+    addressContainer: {
+        width: screenWidth - 100,
+        borderBottomColor: 'red',
+        borderBottomWidth: 0.5,
+        padding: 5,
+        marginBottom: 10,
+        alignItems: 'center',
+    },
+    addressTitle:{
+        fontSize: 24,
+        fontWeight: '700',
+        color: '#7D7D7D'
+    },
+    addressText: {
+        fontSize: 20,
+        fontWeight: '200',
+        color: '#7D7D7D'
+    },
+    footer:{
+        flex: 1,
+    }
 })
